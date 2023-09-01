d37976519e81ff6e28ca51cebc53d3909bf2e667
Ali Ramazanov <aliasifzade@gmail.com>
2023-09-01 22:07:06 +0400

    Set up localStorage for Basket

--- src/store/reducers/basketReducer.js
+++ src/store/reducers/basketReducer.js
@@ -1,23 +1,29 @@
 import { ADD_TO_BASKET, REMOVE_FROM_BASKET } from "../actions/actionTypes";
+
 const initialState = {
-  basket: [],
+  basket: JSON.parse(localStorage.getItem("basket")) || [],
 };
 
 export const basketReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_BASKET:
+      const addedBasket = [...state.basket, action.payload.product];
+      localStorage.setItem("basket", JSON.stringify(addedBasket));
+
       return {
         ...state,
-        basket: [...state.basket, action.payload.product],
+        basket: addedBasket,
       };
     case REMOVE_FROM_BASKET:
+      const filteredBasket = state.basket.filter(
+        (item) => item.id !== action.payload.productId
+      );
+      localStorage.setItem("basket", JSON.stringify(filteredBasket));
+
       return {
         ...state,
-        basket: state.basket.filter(
-          (item) => item.id !== action.payload.productId
-        ),
+        basket: filteredBasket,
       };
-
     default:
       return state;
   }
